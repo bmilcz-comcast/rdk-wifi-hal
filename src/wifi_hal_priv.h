73e1a668ad9b606a880452bd81b755348d260d81
Brayan Milczarek <brayan_milczarek@comcast.com>
2026-09-04 16:22:16 +0200

    RDKB-66691: Fix phy info log floods
    Reason for change: Reworked functions so they stop flooding the logs.
    Increase guarding of params depending on bands - now frequencies may be
    parsed only if radio structure matches.
    Test Procedure: Verify build
    Risks: None
    Priority: P2
    
    Signed-off-by: Brayan Milczarek <brayan_milczarek@comcast.com>

--- src/wifi_hal_priv.h
+++ src/wifi_hal_priv.h
@@ -1474,6 +1474,7 @@ int wifi_channel_to_freq(const char* country, UCHAR opclass, uint channel, uint 
 int bw_enum_to_mhz(wifi_channelBandwidth_t chwid);
 uint16_t freq_to_primary(uint16_t freq, wifi_channelBandwidth_t chwid);
 enum nl80211_band wifi_freq_band_to_nl80211_band(wifi_freq_bands_t band);
+int get_band_index(enum nl80211_band band);
 enum nl80211_band get_nl80211_band_from_rdk_radio_index(unsigned int rdk_radio_index);
 const char* get_chan_dfs_state(struct hostapd_channel_data *chan);
 
